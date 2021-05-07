98a86027a578e976ffd405ba3c4f739073a09ef7
MichaelCriollo <alejo9851@gmail.com>
2021-05-06 20:17:58 -0500

    App Paises

---
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-region',
+  templateUrl: './por-region.component.html',
+  styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `
+  ]
+})
+export class PorRegionComponent implements OnInit {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  hayError: boolean = false;
+  paises  : Country[] = [];
+  
+  constructor(private paisService: PaisService) { }
+
+  ngOnInit(): void {
+  }
+
+  getClaseCss( region: string ): string{
+    return ( region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
+
+  
+  activarRegion( region: string){
+    
+    if(region === this.regionActiva){ return; }
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.getPaisesPorRegion( region )
+      .subscribe( (paises) => {
+        this.paises = paises;
+      }, (err) => {
+        this.hayError = true;
+        this.paises   = [];
+      });
+  }
+
+}
